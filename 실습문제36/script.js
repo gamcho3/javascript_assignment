2ca6253f7385f0eae9d4687d30f2e9192561d783
yongjin park <70016523+gamcho3@users.noreply.github.com>
2024-08-08 21:12:58 +0900

    ass 36

---
+++ 실습문제36/script.js
@@ -0,0 +1,12 @@
+const minji = { id: 21, name: "민지" };
+const hani = { id: 12, name: "하니" };
+const heyin = { id: 24, name: "혜인" };
+
+const users = [minji, hani, heyin];
+
+const posts = [
+  { userId: 21, id: 1, content: "안녕하세요. 민지입니다." },
+  { userId: 12, id: 2, content: "안녕하세요. 하니입니다.1" },
+  { userId: 12, id: 3, content: "안녕하세요. 하니입니다.2" },
+  { userId: 24, id: 4, content: "안녕하세요. 혜인입니다." },
+];
