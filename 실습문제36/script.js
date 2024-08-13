9d05e93320169198f7c0d9b1391567e63d07aafa
yongjin park <70016523+gamcho3@users.noreply.github.com>
2024-08-13 09:08:42 +0900

    add reuslt

--- 실습문제36/script.js
+++ 실습문제36/script.js
@@ -10,3 +10,41 @@ const posts = [
   { userId: 12, id: 3, content: "안녕하세요. 하니입니다.2" },
   { userId: 24, id: 4, content: "안녕하세요. 혜인입니다." },
 ];
+
+function getUser(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const user = users.find((user) => user.id === id);
+      if (user) {
+        resolve(user);
+      } else {
+        reject("사용자를 찾을 수 없습니다.");
+      }
+    }, 1000);
+  });
+}
+
+function getPosts(userId) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const _posts = posts.filter((post) => post.userId === userId);
+      if (_posts.length > 0) {
+        resolve(_posts);
+      } else {
+        reject("포스트를 찾을 수 없습니다.");
+      }
+    }, 1000);
+  });
+}
+
+getUser(21)
+  .then((user) => {
+    console.log(user);
+    return getPosts(user.id);
+  })
+  .then((posts) => {
+    console.log(posts);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
