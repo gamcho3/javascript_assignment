89983758bb129675a897fe0d4b1bac05da3dc993
yongjin park <70016523+gamcho3@users.noreply.github.com>
2024-08-21 21:06:05 +0900

    Create App.jsx

---
+++ 실습문제39/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div id="app">
+      <p>
+        <code>&lt;MainGoal&gt;</code> 컴포넌트를 만들고 이 텍스트 아래에
+        삽입하세요.
+      </p>
+      <p>
+        <code>&lt;MainGoal&gt;</code> 컴포넌트는 학습 목표를 설명하는 텍스트를
+        출력합니다. (예: &quot;나의 주요 목표: React를 자세히 배우기&quot;).
+      </p>
+      <p>
+        <strong>중요:</strong> 커스텀 컴포넌트는 반드시 &quot;나의 주요
+        목표:&quot;라는 텍스트를 포함해야 합니다.
+      </p>
+    </div>
+  );
+}
