7efde1a004cd2d2f580c1cbe32188c409504d5b4
yongjin park <70016523+gamcho3@users.noreply.github.com>
2024-08-26 20:54:42 +0900

    add assignment

---
+++ 실습문제41/assignment/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div id="app" data-testid="app">
+      <h1>리액트 연습하기</h1>
+      <p>목표 설정하기</p>
+      <ul></ul>
+    </div>
+  );
+}
+
+export default App;
