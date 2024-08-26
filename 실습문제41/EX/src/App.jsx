3a41cb623167cd0af837ee749169b28f88ffb065
yongjin park <70016523+gamcho3@users.noreply.github.com>
2024-08-26 20:28:10 +0900

    add ex

---
+++ 실습문제41/EX/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>React Props 연습</h1>
+      <div className="card-container"></div>
+    </div>
+  );
+}
+
+export default App;
