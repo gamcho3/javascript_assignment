8af670bcd8dde03a9d76f84159e8ae18c8da4de2
yongjin park <70016523+gamcho3@users.noreply.github.com>
2024-08-27 15:02:32 +0900

    react

---
+++ 실습문제42/App.jsx
@@ -0,0 +1,22 @@
+const quotes = [];
+
+function App() {
+  return (
+    <div>
+      <header>
+        <ul className="quotes">
+          <li>루즈벨트</li>
+          <li>셰익스피어</li>
+          <li>노자</li>
+        </ul>
+      </header>
+      <main>
+        <h1>오늘의 명언</h1>
+        <div>명언</div>
+        <div>작가</div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
