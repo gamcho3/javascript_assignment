096e7b232ed645992b836969921aada617d4afa7
gamcho3 <70016523+gamcho3@users.noreply.github.com>
2024-08-28 14:36:50 +0900

    fix

--- 실습문제42/EX/src/App.jsx
+++ 실습문제42/EX/src/App.jsx
@@ -1,43 +1,67 @@
-const quotes = [{
-  quotes: "우리가 두려워해야 할 유일한 것은 두려움 그 자체이다.",
-  authors: "프랭클린 D. 루즈벨트",
-},
-{
-  quotes: "사느냐, 죽느냐 그것이 문제로다.",
-  authors: "윌리엄 셰익스피어",
-},
-{ quotes: "천 리 길도 한 걸음부터.", authors: "노자" },];
+/**
+ * Props 구성
+ * props: 컴포넌트에 데이터를 전달하는 역할
+ * props : html attribute(속성)
+ * react는 props를 하나의 Object로 묶어서 전달
+ * props는 컴포넌트 함수에 첫번째 인수로 전달된다.
+ * attribute를통해 값을 전달하고 속성의 이름은 자유롭게 정할수있다
+ * 글자,숫자,객체,배열등을 전달할수 있다
+ * 
+ */
 
-function LiList({authors}){
-  return <li>{authors}</li>
+import React from "react";
+import componentImg from "./assets/components.png";
+import confgImg from "./assets/config.png";
+import jsxImg from "./assets/jsx-ui.png";
+/**
+ * 자바스크립트 구조분해(destructuring)
+ * 
+ */
+function Card({title : subject,content : body,backgroundColor,textColor,img, array = ["react"]}){
+  return <div className="card"
+  style={{backgroundColor:backgroundColor,color:textColor}}
+  >
+  <img src={img} alt="" />
+  <h2 className="card-title">{subject}</h2>
+  <p className="card-content">{body}</p>
+  <p>{array[0]}</p>
+</div>
 }
 
-function Header(){
-  return <header>
-  <ul className="quotes">
-    <LiList authors={quotes[0].authors}/>
-    <LiList authors={quotes[1].authors}/>
-    <LiList authors={quotes[2].authors}/>
-  </ul>
- </header>
-}
-
-function Main({quotes,authors}){
-  // quotes -> Object
-  return <main>
-  <h1>오늘의 명언</h1>
-  <div>{quotes}</div>
-  <div>{authors}</div>
-</main>
-}
-
+// 컴포넌트 분리
 function App() {
-  let randomInt = Math.floor(Math.random() * quotes.length)
-  // 0~2
   return (
-    <div>
-      <Header/>
-      <Main {...quotes[randomInt]}/>
+    <div className="app">
+      <h1>React Props 연습</h1>
+      <div className="card-container" >
+       <Card 
+       title="리액트 props" 
+       content="react props는 컴포넌트에 데이터를 전달하는 방법입니다."
+       backgroundColor="#ffddc1"
+       textColor='#333'
+       img={componentImg}
+       array={['자바스크립트']}
+     
+       />
+       <Card 
+       title="Reusable Component" 
+       content="Card 컴포넌트를 재사용하여 다양한 데이터를 보여줄 수 있습니다."
+       backgroundColor="#c1d7ff"
+       textColor='#333'
+       img={confgImg}
+       array={['플러터']}
+   
+       />
+       <Card 
+       title="Dynamic Styling" 
+       content="props를 이용해 동적으로 스타일을 변경할 수 있습니다."
+       backgroundColor="#d1ffc1"
+       textColor='#333'
+       img={jsxImg}
+      
+       />
+       
+      </div>
     </div>
   );
 }
